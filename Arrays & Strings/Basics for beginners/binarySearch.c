6dee61ecdb9d64235688f928e17aff589887563e
itsomrai <work.omrai@gmail.com>
2026-06-11 22:21:43 +0530

    basics for beginners(binary Search)

---
+++ Arrays & Strings/Basics for beginners/binarySearch.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+int main()
+{
+int arr[]={10,20,30,40,50};//Must be sorted
+int size=sizeof(arr)/sizeof(arr[0]);
+int target=40;
+
+int low=0;
+int high = size-1;
+int foundIndex=-1;
+
+while(low<=high){
+    int mid=(low+high)/2;
+     
+    if(arr[mid]==target){
+        foundIndex=mid;
+        break;//Stop looping immediately
+    }
+    else if(arr[mid] < target){
+        low=mid+1; //Search right half
+    }
+    else{
+        high = mid-1;//search left half
+    }
+
+}
+if(foundIndex!=-1){
+    printf("found at index %d\n",foundIndex);
+}
+else{
+    printf("Not found");
+}
+return 0;
+}
